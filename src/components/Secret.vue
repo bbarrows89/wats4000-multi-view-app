3fd14de3d3ef3d541fc8704a72815c536b440445
Bryan Barrows <bryan.barrows@seattlecolleges.edu>
2018-02-07 08:56:51 -0800

    created 'Secret' view and imported/added to router

---
+++ src/components/Secret.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="component">
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        message: "Don't be afraid to read the docs!"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .component {
+    background: #e8e8e8;
+    min-height: 400px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Survey from '@/components/Survey'
-// TODO: Import the Secret component
+import Secret from '@/components/Secret'
 
 Vue.use(Router)
 
@@ -16,8 +16,12 @@ export default new Router({
     {
       path: '/survey',
       name: 'Survey',
-      component: Survey,
+      component: Survey
+    },
+    {
+      path: '/secret',
+      name: 'Secret',
+      component: Secret
     }
-    // Add the Secret route definition
   ]
 })
